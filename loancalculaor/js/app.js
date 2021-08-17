93a51cc28eec2a6abda2fc0d4de4ddb506a01588
linnminhtet23 <linnminhtet23@gmail.com>
2021-08-17 12:36:52 +0630

    initial commit

--- loancalculaor/js/app.js
+++ loancalculaor/js/app.js
@@ -1,15 +1,20 @@
+//UI
 const terms = document.getElementById('years');
 const bubble = document.querySelector('.bubble');
 
 const loanform = document.getElementById('loan-form');
 const loader =  document.getElementById('loading')
+const resultel = document.getElementById('result');
 
 //Event Listener
 terms.addEventListener('input', function(){
-    const val =terms.nodeValue;
-    bubble.textContent= value>1 ? `${val} Months`: `${val} Month`;
+    const val =terms.value;
+    bubble.textContent= val>1 ? `${val} Months`: `${val} Month`;
 });
+
 loanform.addEventListener('submit', function(e){
+    //hide result
+    resultel.style.display='none'
     // show loader
     loader.style.display = 'block';
     
@@ -32,4 +37,55 @@ function calculateresult(){
     const getinterest =  parseFloat(interest.value)/12;//for one year divided by 12
     const getterm = parseFloat(terms.value);// get the number of month from range 
     // console.log(getprincipal);
+
+    // compute all monthly payment 
+    const monthly = Math.round((getprincipal*(getterm*getinterest))/100);
+    // console.log(monthly);
+
+    if(monthly){
+        //toFixed is use to change number to fixed point notation
+        monthlypayment.value =((getprincipal+monthly)/getterm).toFixed(2);
+        totalinterest.value = monthly.toFixed(2);
+        totalpayment.value = (monthlypayment.value*getterm).toFixed(2);
+
+        //Hide Loader
+        loader.style.display='none';
+
+        //show result
+        resultel.style.display='block';
+
+    }else{
+        // console.log('Please check your number')
+        showerror('Please check your number');
+
+    }
+}
+
+//Show Error Message
+function showerror(message){
+    //hide loader
+    loader.style.display = 'none';
+    
+    // createelement
+    const errordiv = document.createElement('div');
+    // add class 
+    errordiv.className = 'alert alert-danger';
+    // add text node 
+    errordiv.appendChild(document.createTextNode(message));
+
+    // get elements
+    const card = document.querySelector('.card');
+    const heading = document.getElementById('heading');
+
+    //insert error above heading(up, down)
+    card.insertBefore(errordiv, heading);
+
+    // console.log(errordiv);
+    //clear errordiv after display
+    setTimeout(clearerror,2000);
+}
+
+//Clear Error
+function clearerror(){
+    document.querySelector('.alert').remove();
 }
